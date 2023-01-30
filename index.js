58c95ca378977bc9b1676dfa9c68212bb68fdba1
Thomas Wallingford <tbwallingford@gmail.com>
2023-01-30 12:47:36 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Randall Munroe';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
