ab434ced82fe5fb03701558792154370c67de1e2
Thomas Wallingford <tbwallingford@gmail.com>
2023-01-30 13:00:45 -0700

    lab as intended

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = 'Randall Munroe';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
-const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
